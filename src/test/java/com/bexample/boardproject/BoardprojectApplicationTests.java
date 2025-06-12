31f8d058ab065e27b532fb4229716067349ce295
kcas1005 <kcas1005@naver.com>
2025-06-12 16:15:49 +0900

    스프링 연습용

---
+++ src/test/java/com/bexample/boardproject/BoardprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bexample.boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
